35599ef74a7e59a51e6a506102e247cf647ac0a0
edoc99 <harshitjainf3@gmail.com>
2020-01-11 16:33:59 +0530

    created login function

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 
 def signup_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            # TODO: log the user
+            user = form.save()
+            # log the user
+            login(request, user)
             return redirect('articles:list')
     else:
         form = UserCreationForm()
@@ -18,7 +20,9 @@ def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            # TODO: LOg the user
+            # Log the user
+            user = form.get_user()
+            login(request, user)
             return redirect('articles:list')
     else:
         form = AuthenticationForm()
