1c43fa248ae9d0f6e1f0ce3bfb9439049671ab46
edoc99 <harshitjainf3@gmail.com>
2020-01-11 11:03:06 +0530

    created named url

--- articles/urls.py
+++ articles/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'articles'
 
 urlpatterns = [
-    path('', views.article_list),
+    path('', views.article_list, name="list"),
     # named capturing group
-    path('<slug:slug>/', views.article_detail),
+    path('<slug:slug>/', views.article_detail, name="detail"),
 ]
